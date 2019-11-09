b6dfabed78400fe244cf7c6effb771c86bf5944a
Gmiliuta <53524766+Gmiliuta@users.noreply.github.com>
2019-11-09 10:52:31 +0100

    heroku env configs

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONDGODB, {
+mongoose.connect(process.env.MONGODB, {
   useNewUrlParser:true,
   useUnifiedTopology: true
 }, () => {
